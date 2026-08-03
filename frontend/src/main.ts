c3553b33f3e155f7d90f76f94e74020f231d8134
liveinaus <admin@liveinaus.com>
2026-08-03 13:04:18 +1000

    fix: MS Edge minimise issue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { suppressEdgeMinimizeRestore } from './utils/edgeMinimize';
 import './style.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 (async () => {
+  suppressEdgeMinimizeRestore();
   const app = createApp(App);
   app.use(router);
   await router.isReady();
